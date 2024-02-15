738e08695e86d85b5f1d1227263bbe2d5e0a4786
Mayke Palma <mayketjs@gmail.com>
2024-02-15 18:10:27 +0000

    fisrt commit

---
+++ js/app.js
@@ -0,0 +1,59 @@
+function comprar () {
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = parseInt(document.getElementById('qtd').value);
+
+    if (tipo.value == 'pista') {
+        comprarPista(qtd);
+
+    } else if (tipo.value == 'superior'){
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd);
+    }
+
+
+
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById ('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo pista');
+    } else {
+        qtdPista =qtdPista - qtd;
+        document.getElementById ('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada');
+    }
+
+
+
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById ('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponível para tipo superior');
+    } else {
+        qtdSuperior =qtdSuperior - qtd;
+        document.getElementById ('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada');
+    }
+
+
+
+}
+
+
+function comprarInferior(qtd) {
+    let qtdiInferior = parseInt(document.getElementById ('qtd-inferior').textContent);
+    if (qtd > qtdiInferior) {
+        alert('Quantidade indisponível para tipo inferior');
+    } else {
+        qtdiInferior =qtdiInferior - qtd;
+        document.getElementById ('qtd-inferior').textContent = qtdiInferior;
+        alert('Compra realizada');
+    }
+
+
+
+}
